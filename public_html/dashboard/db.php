567082b1b83cf5476041806f0cdca1615ddf2ffa
misocioweb <josue@misocioweb.com>
2017-07-11 20:56:11 -0500

    From Phpstorm 2016.1

--- public_html/dashboard/db.php
+++ public_html/dashboard/db.php
@@ -1,5 +1,5 @@
 <?php 
-	$conection=mysql_connect("localhost" , "misocioweb" , "misocioweb.2017") or die(mysql_error());
+	$conection=mysql_connect("localhost" , "root" , "misocioweb.2017") or die(mysql_error());
 	mysql_select_db("bt");
 	include "functions/log.php"
 ?>
