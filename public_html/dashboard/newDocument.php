41160f941ec67f7741f65717321782cc61821599
misocioweb <josue@misocioweb.com>
2017-07-12 20:49:04 -0500

    Trabajos 12/07

--- public_html/dashboard/newDocument.php
+++ public_html/dashboard/newDocument.php
@@ -3,32 +3,34 @@
     Task 0.001 Create a document - Josue Navarrete
 -->
 <html>
-    <head>
-        <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.js"></script>
-        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.0.0-alpha.6/css/bootstrap.css" />
-        <script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.0.0-alpha.6/js/bootstrap.js"></script>
-        <script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
-        <?php
-        $pages=$_REQUEST['pages'];
-        $idTemplate=$_REQUEST['idTemplate'];
-        $elements=$_REQUEST['elements'];
-        //echo "create this $pages > $idTemplate> $elements :)";
-        $id=$idTemplate;
-        include "db.php";
-        $query=mysql_query("SELECT * FROM document_template WHERE id=$id") or die(mysql_error());
-        $f=mysql_fetch_array($query);
-        $idHeader=$f[default_header];
-        $idFooter=$f[default_footer];
-        $queryHeader=mysql_query("SELECT * FROM document_headers WHERE id=$idHeader") or die(mysql_error());
-        $fHeader=mysql_fetch_array($queryHeader);
-        $header=$fHeader[picture];
-        $queryFooter=mysql_query("SELECT * FROM document_footers WHERE id=$idFooter") or die(mysql_error());
-        $fFooter=mysql_fetch_array($queryFooter);
-        $footer=$fFooter[picture];
+<head>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.js"></script>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.0.0-alpha.6/css/bootstrap.css" />
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.0.0-alpha.6/js/bootstrap.js"></script>
+    <script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
+    <?php
+    $pages=$_REQUEST['pages'];
+    $idTemplate=$_REQUEST['idTemplate'];
+    $elements=$_REQUEST['elements'];
+    $elementsPX=abs($elements)."px";
+    //echo "create this $pages > $idTemplate> $elements :)";
+    $id=$idTemplate;
+    include "db.php";
+    $query=mysql_query("SELECT * FROM document_template WHERE id=$id") or die(mysql_error());
+    $f=mysql_fetch_array($query);
+    $idHeader=$f[default_header];
+    $idFooter=$f[default_footer];
+    $queryHeader=mysql_query("SELECT * FROM document_headers WHERE id=$idHeader") or die(mysql_error());
+    $fHeader=mysql_fetch_array($queryHeader);
+    $header=$fHeader[picture];
+    $queryFooter=mysql_query("SELECT * FROM document_footers WHERE id=$idFooter") or die(mysql_error());
+    $fFooter=mysql_fetch_array($queryFooter);
+    $footer=$fFooter[picture];
 
-        ?>
-    </head>
+    ?>
+</head>
 <body>
+
 <div style="height:auto;width:75%;background-color:#EDECEB;position:absolute;left:25%;top:0px;" align="center">
     <?php
     session_start();
@@ -40,7 +42,7 @@
         echo '
 
             <div style="background-color:white;height:842px;width:595px;box-shadow:2px 2px 2px gray; position:relative; " id="a4">
-            <img src="'.$logo.'" id="draggable">
+            <img src="'.$logo.'" id="draggable" style="position:absolute;left:$elementsPX;">
         <img src="'.$header.'" id="header" style="position:absolute;left:0px;top:0px;"/> 
         <img src="'.$footer.'" id="footer" style="position:absolute;left:0px;bottom:0px;"/> 
         </div>   
@@ -49,11 +51,5 @@
     }
     ?>
 </div>
-
 </body>
 </html>
-
-
-
-
-
