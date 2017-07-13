1e6c1e02c40961b59d1d664b5e94d5ddc2286661
misocioweb <josue@misocioweb.com>
2017-07-12 21:07:10 -0500

    Trabajos 12/07

--- public_html/dashboard/t.php
+++ public_html/dashboard/t.php
@@ -13,6 +13,16 @@ $img1=imagecreatefromjpeg($images[0]);
 $img2=imagecreatefrompng($images[1]);
 $img3=imagecreatefrompng($images[2]);
 
+$white = imagecolorallocate($output_image, 255, 255, 255);
+
+imagefill($output_image, 0, 0, $white);
+imagefill($img1, 0, 0, $white);
+imagefill($img2, 0, 0, $white);
+
+$dimensionsLogo = getimagesize($images[2]);
+$widthLogo = $dimensionsLogo[0];
+$heightLogo = $dimensionsLogo[1];
+
 
 
 exit();
