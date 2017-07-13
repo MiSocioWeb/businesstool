0bdcf1a36cbe87bfe0c04f562784b1fd29f1739c
misocioweb <josue@misocioweb.com>
2017-07-12 21:05:35 -0500

    Trabajos 12/07

--- public_html/dashboard/t.php
+++ public_html/dashboard/t.php
@@ -5,6 +5,16 @@ $images = array('up/2.jpg','up/p2.png','up/logo_negro.png');
 
     phpinfo();
 
+$a4=array('595','842');
+$background = imagecreatetruecolor($a4[0], $a4[1]); // setting canvas size
+$output_image = $background;
+
+$img1=imagecreatefromjpeg($images[0]);
+$img2=imagecreatefrompng($images[1]);
+$img3=imagecreatefrompng($images[2]);
+
+
+
 exit();
 
 
